0002f3aec791f4c957dbfd33c23ac260a6cb279d
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-06-02 20:47:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -188,6 +188,20 @@ def update():
             player.SPEED = boostSpeed * 5.5
             player.jump_height = 1.4
 
+        if hit.entity == finishBlock_8:
+            destroyLevel08()
+            player.position = (0, 5, 0)
+
+
+    if ground_9.enabled == True:
+        if hit.entity == block_9_8:
+            player.jump_height = 3
+        elif hit.entity != block_9_8:
+            player.jump_height = normalJump
+        if hit.entity == block_9_11:
+            player.SPEED = boostSpeed * 4
+            player.jump_height = 2
+
 
 
 
@@ -413,6 +427,37 @@ ground_8.disable()
 finishBlock_8.disable()
 
 
+
+# Level09
+ground_9 = StartBlock()
+block_9_1 = NormalBlock(position = (0, 1, 13))
+block_9_2 = FakeBlock(position = (0, 1, 22))
+block_9_3 = NormalBlock(position = (-6, 1, 22))
+block_9_4 = NormalBlock(position = (6, 1, 22))
+block_9_5 = NormalBlock(position = (0, 1, 29))
+block_9_6 = FakeBlock(position = (0, 1, 40))
+block_9_7 = NormalBlock(position = (0, -49, 40))
+block_9_8 = JumpBlock(position = (0, -49, 51))
+block_9_9 = FakeBlock(position = (0, 100, 70))
+block_9_10 = NormalBlock(position = (0, 100, 80))
+block_9_11 = SpeedBlock(position = (0, 100, 90))
+
+finishBlock_9 = EndBlock(position = (0, 150, 300))
+
+ground_9.disable()
+finishBlock_9.disable()
+block_9_1.disable()
+block_9_2.disable()
+block_9_3.disable()
+block_9_4.disable()
+block_9_5.disable()
+block_9_6.disable()
+block_9_7.disable()
+block_9_8.disable()
+block_9_9.disable()
+block_9_10.disable()
+block_9_11.disable()
+
 def destroyLevel01():
     block_1.disable()
     block_1_1.disable()
@@ -613,6 +658,38 @@ def destroyLevel07():
     block_8_11.enable()
     ground_8.enable()
     finishBlock_8.enable()
+
+    player.SPEED = normalSpeed
+    player.jump_height = normalJump
+
+def destroyLevel08():
+    block_8_1.disable()
+    block_8_2.disable()
+    block_8_3.disable()
+    block_8_4.disable()
+    block_8_5.disable()
+    block_8_6.disable()
+    block_8_7.disable()
+    block_8_8.disable()
+    block_8_9.disable()
+    block_8_10.disable()
+    block_8_11.disable()
+    ground_8.disable()
+    finishBlock_8.disable()
+    
+    block_9_1.enable()
+    block_9_2.enable()
+    block_9_3.enable()
+    block_9_4.enable()
+    block_9_5.enable()
+    block_9_6.enable()
+    block_9_7.enable()
+    block_9_8.enable()
+    block_9_9.enable()
+    block_9_10.enable()
+    block_9_11.enable()
+    ground_9.enable()
+    finishBlock_9.enable()
 
     player.SPEED = normalSpeed
     player.jump_height = normalJump
