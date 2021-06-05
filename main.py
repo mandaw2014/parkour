f70ad27f35eb89559f9960c8ff0ccd773acec7aa
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-06-05 20:02:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,12 @@ player.jump_height = normalJump
 # Sky
 sky = Sky(texture = "../assets/sky")
 
+# Audio
+jump = Audio(sound_file_name = "assets/jumping.mp3", autoplay = False)
+jump.volume = 0.5
+land = Audio(sound_file_name = "assets/land.mp3", autoplay = False)
+land.volume = 0.1
+
 # Lighting
 light = PointLight(parent = camera, position = (0, 10, -1.5))
 light.color = color.white
@@ -62,7 +68,14 @@ def update():
 
     # What entity the player hits
     hit = raycast(player.position, player.down, distance = 2, ignore = [player,])
-    
+
+    # Jump sound effect
+    if hit.hit:
+        jump.play()
+
+    if not hit.hit:
+        land.play()
+
     if ground_1.enabled == True:
         if hit.entity == block_1_6:
             player.jump_height = 0.7
@@ -201,6 +214,25 @@ def update():
         if hit.entity == block_9_11:
             player.SPEED = boostSpeed * 4
             player.jump_height = 2
+        if hit.entity == finishBlock_9:
+            destroyLevel09()
+            player.position = (0, 5, 0)
+
+
+    if ground_10.enabled == True:
+        if hit.entity == block_10_2:
+            player.SPEED = boostSpeed * 2.3
+        if hit.entity == block_10_3:
+            player.jump_height = 1.3
+        elif hit.entity != block_10_3:
+            player.jump_height = normalJump
+        if hit.entity == block_10_5 or hit.entity == block_10_7 or hit.entity == block_10_9 or hit.entity == block_10_10 or hit.entity == block_10_11 or hit.entity == block_10_12 or hit.entity == block_10_13 or hit.entity == block_10_14 or hit.entity == block_10_15 or hit.entity == block_10_16 or hit.entity == block_10_17 or hit.entity == block_10_18 or hit.entity == block_10_19 or hit.entity == block_10_20:
+            player.SPEED = normalSpeed
+            player.jump_height = normalJump
+        if hit.entity == block_10_21:
+            camera.rotation_z = 180
+        if hit.entity == finishBlock_10:
+            camera.rotation_z = 0
 
 
 
@@ -458,6 +490,69 @@ block_9_9.disable()
 block_9_10.disable()
 block_9_11.disable()
 
+
+
+# Level 10
+ground_10 = StartBlock()
+block_10_1 = NormalBlock(position = (0, 1, 13))
+block_10_2 = SpeedBlock(position = (0, 1, 25))
+block_10_3 = JumpBlock(position = (0, -10, 70))
+block_10_4 = FakeBlock(position = (0, 10, 150))
+block_10_5 = NormalBlock(position = (0, 10, 160))
+block_10_6 = Wall(position = (0, 11, 165))
+block_10_7 = NormalBlock(position = (0, 10, 170))
+block_10_8 = Wall(position = (0, 11, 175))
+block_10_9 = NormalBlock(position = (0, 10, 180))
+block_10_10 = NormalBlock(position = (0, 10, 190))
+block_10_11 = NormalBlock(position = (-5, 11, 190))
+block_10_12 = NormalBlock(position = (-10, 12, 190))
+block_10_13 = NormalBlock(position = (-10, 13, 185))
+block_10_14 = NormalBlock(position = (-10, 14, 180))
+block_10_15 = NormalBlock(position = (-10, 15, 175))
+block_10_16 = NormalBlock(position = (-5, 16, 175))
+block_10_17 = NormalBlock(position = (0, 17, 175))
+block_10_18 = NormalBlock(position = (0, 18, 185))
+block_10_19 = NormalBlock(position = (0, 19, 195))
+block_10_20 = NormalBlock(position = (0, 20, 205))
+block_10_21 = WeirdBlock(position = (0, 20, 220))
+block_10_22 = NormalBlock(position = (0, 20, 233))
+block_10_23 = NormalBlock(position = (0, 20, 243))
+block_10_24 = NormalBlock(position = (0, 20, 253))
+block_10_25 = NormalBlock(position = (0, 20, 263))
+
+finishBlock_10 = EndBlock(position = (0, 20, 275))
+
+ground_10.disable()
+finishBlock_10.disable()
+block_10_1.disable()
+block_10_2.disable()
+block_10_3.disable()
+block_10_4.disable()
+block_10_5.disable()
+block_10_6.disable()
+block_10_7.disable()
+block_10_8.disable()
+block_10_9.disable()
+block_10_10.disable()
+block_10_11.disable()
+block_10_12.disable()
+block_10_13.disable()
+block_10_14.disable()
+block_10_15.disable()
+block_10_16.disable()
+block_10_17.disable()
+block_10_18.disable()
+block_10_19.disable()
+block_10_20.disable()
+block_10_21.disable()
+block_10_22.disable()
+block_10_23.disable()
+block_10_24.disable()
+block_10_25.disable()
+
+
+
+
 def destroyLevel01():
     block_1.disable()
     block_1_1.disable()
@@ -690,6 +785,54 @@ def destroyLevel08():
     block_9_11.enable()
     ground_9.enable()
     finishBlock_9.enable()
+
+    player.SPEED = normalSpeed
+    player.jump_height = normalJump
+
+
+def destroyLevel09():
+    block_9_1.disable()
+    block_9_2.disable()
+    block_9_3.disable()
+    block_9_4.disable()
+    block_9_5.disable()
+    block_9_6.disable()
+    block_9_7.disable()
+    block_9_8.disable()
+    block_9_9.disable()
+    block_9_10.disable()
+    block_9_11.disable()
+    ground_9.disable()
+    finishBlock_9.disable()
+
+
+    ground_10.enable()
+    finishBlock_10.enable()
+    block_10_1.enable()
+    block_10_2.enable()
+    block_10_3.enable()
+    block_10_4.enable()
+    block_10_5.enable()
+    block_10_6.enable()
+    block_10_7.enable()
+    block_10_8.enable()
+    block_10_9.enable()
+    block_10_10.enable()
+    block_10_11.enable()
+    block_10_12.enable()
+    block_10_13.enable()
+    block_10_14.enable()
+    block_10_15.enable()
+    block_10_16.enable()
+    block_10_17.enable()
+    block_10_18.enable()
+    block_10_19.enable()
+    block_10_20.enable()
+    block_10_21.enable()
+    block_10_22.enable()
+    block_10_23.enable()
+    block_10_24.enable()
+    block_10_25.enable()
 
     player.SPEED = normalSpeed
     player.jump_height = normalJump
