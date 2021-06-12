5bad0f050edb773986f2283c98836ce0ce1ca770
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-06-12 11:57:09 +0100

    made main menu cleaner

--- main_menu.py
+++ main_menu.py
@@ -1,79 +1,28 @@
 from ursina import *
 
-# Main Menu Example, or it can be any kind of menu, like Inventory, Quest journal, etc.
-# Created by Doctor
-# 09 Feb 21
+app = Ursina()
 
-# Class of game menu
-# MainMenu
 class MainMenu(Entity):
-    def __init__(self, **kwargs):
-        super().__init__(parent=camera.ui, ignore_paused=True)
+    def __init__(self):
+        super().__init__(
+            parent = camera.ui
+        )
 
-        self.main_menu = Entity(parent=self, enabled=True)
-        self.options_menu = Entity(parent=self, enabled=False)
-        self.help_menu = Entity(parent=self, enabled=False)
+        self.main_menu = Entity(parent = self, enabled = True)
 
-        Text("MAIN MENU", parent=self.main_menu, y=0.4, x=0, origin=(0,0))
-
-        def quit_game():
-            application.quit()
-
-        def options_menu_btn():
-            self.options_menu.enable()
+        def start():
+            player.enable()
+            mouse.locked = True
             self.main_menu.disable()
 
-        def help_menu_btn():
-            self.help_menu.enable()
-            self.main_menu.disable()
+        title = Entity(model = "quad", scale = (0.65, 0.2, 0.2), texture = "assets/parkour_logo_4", parent = self.main_menu, y = 0.3)
 
-        ButtonList(button_dict={
-            "Start": Func(print_on_screen,"You clicked on the Start button!", position=(0,.1), origin=(0,0)),
-            "Options": Func(options_menu_btn),
-            "Help": Func(help_menu_btn),
-            "Exit": Func(quit_game)
-        },y=0,parent=self.main_menu)
+        start_button = Button(text = "S t a r t - G a m e", color = color.black, scale_y = 0.1, scale_x = 0.3, y = -0.1, parent = self.main_menu)
+        quit_button = Button(text = "Q u i t", color = color.black, scale_y = 0.1, scale_x = 0.3, y = -0.22, parent = self.main_menu)
+        quit_button.on_click = application.quit
+        start_button.on_click = Func(start)
 
-        Text ("OPTIONS MENU", parent=self.options_menu, y=0.4, x=0, origin=(0, 0))
 
-        def options_back_btn_action():
-            self.main_menu.enable()
-            self.options_menu.disable()
+m = MainMenu()
 
-        Button("Back",parent=self.options_menu,y=-0.3,scale=(0.1,0.05),color=rgb(50,50,50),
-               on_click=options_back_btn_action)
-
-        Text ("HELP MENU", parent=self.help_menu, y=0.4, x=0, origin=(0, 0))
-
-        def help_back_btn_action():
-            self.main_menu.enable()
-            self.help_menu.disable()
-
-        ButtonList (button_dict={
-            "Gameplay": Func(print_on_screen,"You clicked on Gameplay help button!", position=(0,.1), origin=(0,0)),
-            "Battle": Func(print_on_screen,"You clicked on Battle help button!", position=(0,.1), origin=(0,0)),
-            "Control": Func(print_on_screen,"You clicked on Control help button!", position=(0,.1), origin=(0,0)),
-            "Back": Func (help_back_btn_action)
-        }, y=0, parent=self.help_menu)
-        
-        for key, value in kwargs.items ():
-            setattr (self, key, value)
-
-    def input(self, key):
-        if self.main_menu.enabled:
-            if key == "escape":
-                # Close app
-                application.quit()
-
-        if self.options_menu.enabled:
-            if key == "escape":
-                self.main_menu.enable()
-                self.options_menu.disable()
-
-        if self.help_menu.enabled:
-            if key == "escape":
-                self.main_menu.enable()
-                self.help_menu.disable()
-
-    def update(self):
-        pass
+app.run()
