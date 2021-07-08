08117e641017a7b6f21ed4d0ed8a3a417e0ed55f
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-07-08 21:50:53 +0100

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -1,7 +1,5 @@
 from ursina import *
 
-app = Ursina()
-
 class MainMenu(Entity):
     def __init__(self):
         super().__init__(
@@ -9,20 +7,16 @@ class MainMenu(Entity):
         )
 
         self.main_menu = Entity(parent = self, enabled = True)
+        self.player = None
 
         def start():
-            player.enable()
+            self.player.enable()
             mouse.locked = True
             self.main_menu.disable()
 
-        title = Entity(model = "quad", scale = (0.65, 0.2, 0.2), texture = "assets/parkour_logo_4", parent = self.main_menu, y = 0.3)
+        title = Entity(model = "quad", scale = (0.8, 0.2, 0.2), texture = "assets/parkour_logo_4", parent = self.main_menu, y = 0.3)
 
         start_button = Button(text = "S t a r t - G a m e", color = color.black, scale_y = 0.1, scale_x = 0.3, y = -0.1, parent = self.main_menu)
         quit_button = Button(text = "Q u i t", color = color.black, scale_y = 0.1, scale_x = 0.3, y = -0.22, parent = self.main_menu)
         quit_button.on_click = application.quit
         start_button.on_click = Func(start)
-
-
-m = MainMenu()
-
-app.run()
