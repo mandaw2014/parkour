98a46bf4c37acd459f1307c6ed567eead4fb65ea
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-07-13 09:49:19 +0100

    added level04

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from Levels.level01 import Level01
 from Levels.level02 import Level02
 from Levels.level03 import Level03
 
+from Levels.level04 import Level04
+
 from main_menu import MainMenu
 from pause_menu import PauseMenu
 
@@ -30,7 +32,7 @@ player.SPEED = normalSpeed
 player.jump_height = normalJump
 player.disable()
 player.position = (888, 10, 18)
-player.rotation = (0, 181, 0)
+player.rotation = (0, -142, 0)
 
 mouse.locked = False
 
@@ -40,7 +42,7 @@ m.player = player
 sky = Sky(texture = "./assets/sky")
 
 # Lighting
-PointLight(parent = camera, position = (0, 10, -1.5), color = color.white)
+light = PointLight(parent = camera, position = (0, 10, -1.5), color = color.white)
 AmbientLight(color = color.rgba(100, 100, 100, 0.1))
 
 level01 = Level01()
@@ -51,6 +53,9 @@ level02.player = player
 
 level03 = Level03()
 level03.player = player
+
+level04 = Level04()
+level04.player = player
 
 def reset_player():
     player.position = (0, 3, 0)
@@ -74,6 +79,8 @@ def update():
     if ray.entity == level01.finishBlock_1:
         player.position = (811, 14, 108)
         player.rotation = (0, -267, 0)
+        player.SPEED = normalSpeed
+        player.jump_height = normalJump
 
         level01.disable()
         level02.enable()
@@ -81,9 +88,20 @@ def update():
     if ray.entity == level02.finishBlock_2:
         player.position = (809, 4, 106)
         player.rotation = (0, 181, 0)
+        player.SPEED = normalSpeed
+        player.jump_height = normalJump
 
         level02.disable()
         level03.enable()
+    
+    if ray.entity == level03.finishBlock_3:
+        player.position = (5, 10, -150)
+        player.rotation = (0, -90, 0)
+        player.SPEED = normalSpeed
+        player.jump_height = normalJump
+
+        level03.disable()
+        level04.enable()
 
     print(round(player.position, 0), round(player.rotation, 0))
 
