e85329f7cd3abcf10bed74c3bab0c69251ba07eb
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-07-16 11:12:12 +0100

    added level06

--- main_menu.py
+++ main_menu.py
@@ -15,6 +15,7 @@ class MainMenu(Entity):
         self.level03 = None
         self.level04 = None
         self.level05 = None
+        self.level06 = None
 
         def start():
             self.player.enable()
@@ -81,6 +82,18 @@ class MainMenu(Entity):
             self.level01.disable()
             self.level05.enable()
 
+        def level06_menu():
+            self.main_menu.disable()
+            self.levels_menu.disable()
+            self.player.enable()
+            self.player.time_running = True
+            self.player.position = (130, -120, 0)
+            self.player.rotation = (0, -90, 0)
+            mouse.locked = True
+
+            self.level01.disable()
+            self.level06.enable()
+
         def back():
             self.main_menu.enable()
             self.levels_menu.disable()
@@ -100,6 +113,7 @@ class MainMenu(Entity):
 
         level04 = Button(text = "L e v e l 0 4", color = color.black, scale_y = 0.1, scale_x = 0.3, y = 0, x = -0.5, parent = self.levels_menu)
         level05 = Button(text = "L e v e l 0 5", color = color.black, scale_y = 0.1, scale_x = 0.3, y = 0, x = 0, parent = self.levels_menu)
+        level06 = Button(text = "L e v e l 0 6", color = color.black, scale_y = 0.1, scale_x = 0.3, y = 0, x = 0.5, parent = self.levels_menu)
 
         back_button = Button(text = "< - B a c k", color = color.gray, scale_y = 0.1, scale_x = 0.2, y = -0.4, x = -0.5, parent = self.levels_menu)
 
@@ -108,5 +122,6 @@ class MainMenu(Entity):
         level03.on_click = Func(level03_menu)
         level04.on_click = Func(level04_menu)
         level05.on_click = Func(level05_menu)
+        level06.on_click = Func(level06_menu)
 
-        back_button.on_click = Func(back)
+        back_button.on_click = Func(back)a
