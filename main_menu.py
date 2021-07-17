9df47bb24cb5644f4d4772d2281effad05aa3cd0
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-07-17 15:31:32 +0100

    added all of the levels

--- main_menu.py
+++ main_menu.py
@@ -16,6 +16,9 @@ class MainMenu(Entity):
         self.level04 = None
         self.level05 = None
         self.level06 = None
+        self.level07 = None
+        self.level08 = None
+        self.level09 = None
 
         def start():
             self.player.enable()
@@ -94,11 +97,47 @@ class MainMenu(Entity):
             self.level01.disable()
             self.level06.enable()
 
+        def level07_menu():
+            self.main_menu.disable()
+            self.levels_menu.disable()
+            self.player.enable()
+            self.player.time_running = True
+            self.player.position = (0, 10, 0)
+            self.player.rotation = (0, 0, 0)
+            mouse.locked = True
+
+            self.level01.disable()
+            self.level07.enable()
+
+        def level08_menu():
+            self.main_menu.disable()
+            self.levels_menu.disable()
+            self.player.enable()
+            self.player.time_running = True
+            self.player.position = (0, 10, 0)
+            self.player.rotation = (0, 0, 0)
+            mouse.locked = True
+
+            self.level01.disable()
+            self.level08.enable()
+
+        def level09_menu():
+            self.main_menu.disable()
+            self.levels_menu.disable()
+            self.player.enable()
+            self.player.time_running = True
+            self.player.position = (0, 10, 0)
+            self.player.rotation = (0, 0, 0)
+            mouse.locked = True
+
+            self.level01.disable()
+            self.level09.enable()
+
         def back():
             self.main_menu.enable()
             self.levels_menu.disable()
 
-        title = Entity(model = "quad", scale = (0.8, 0.2, 0.2), texture = "assets/parkour_logo_4", parent = self.main_menu, y = 0.3)
+        title = Entity(model = "quad", scale = (0.8, 0.2, 0.2), texture = "parkour_logo_4", parent = self.main_menu, y = 0.3)
 
         start_button = Button(text = "S t a r t - G a m e", color = color.black, scale_y = 0.1, scale_x = 0.3, y = 0.02, parent = self.main_menu)
         levels_button = Button(text = "L e v e l s", color = color.black, scale_y = 0.1, scale_x = 0.3, y = -0.1, parent = self.main_menu)
@@ -115,7 +154,11 @@ class MainMenu(Entity):
         level05 = Button(text = "L e v e l 0 5", color = color.black, scale_y = 0.1, scale_x = 0.3, y = 0, x = 0, parent = self.levels_menu)
         level06 = Button(text = "L e v e l 0 6", color = color.black, scale_y = 0.1, scale_x = 0.3, y = 0, x = 0.5, parent = self.levels_menu)
 
-        back_button = Button(text = "< - B a c k", color = color.gray, scale_y = 0.1, scale_x = 0.2, y = -0.4, x = -0.5, parent = self.levels_menu)
+        level07 = Button(text = "L e v e l 0 7", color = color.black, scale_y = 0.1, scale_x = 0.3, y = -0.3, x = -0.5, parent = self.levels_menu)
+        level08 = Button(text = "L e v e l 0 8", color = color.black, scale_y = 0.1, scale_x = 0.3, y = -0.3, x = 0, parent = self.levels_menu)
+        level09 = Button(text = "L e v e l 0 9", color = color.black, scale_y = 0.1, scale_x = 0.3, y = -0.3, x = 0.5, parent = self.levels_menu)
+
+        back_button = Button(text = "< - B a c k", color = color.gray, scale_y = 0.05, scale_x = 0.2, y = 0.45, x = -0.65, parent = self.levels_menu)
 
         level01.on_click = Func(level01_menu)
         level02.on_click = Func(level02_menu)
@@ -123,5 +166,8 @@ class MainMenu(Entity):
         level04.on_click = Func(level04_menu)
         level05.on_click = Func(level05_menu)
         level06.on_click = Func(level06_menu)
+        level07.on_click = Func(level07_menu)
+        level08.on_click = Func(level08_menu)
+        level09.on_click = Func(level09_menu)
 
-        back_button.on_click = Func(back)a
+        back_button.on_click = Func(back)
